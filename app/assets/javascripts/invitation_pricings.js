0536bccf14aa829b7b4f95379f9db4afc5cd5b2c
jesseocon <jesse.ocon@oconcompany.com>
2012-12-14 16:03:45 -0800

    finished the javascript logic for the invitation options and quantity box. Still need to refactor the sumAll function but not super important at this point.

--- app/assets/javascripts/invitation_pricings.js
+++ app/assets/javascripts/invitation_pricings.js
@@ -1,50 +1,29 @@
 $(document).ready(function(){
     if ( $("#invitation_invitation_pricing_id").length > 0 ){
         var id = "#invitation_invitation_pricing_id";
-        getTotal(id);
+        setTotal(id);
     }
     
-    $('#invitation_invitation_pricing_id').change(function(){
-        getTotal();
-    });
-    
-    $(".price").change(function(){
+    $(".separator select").change(function(){
         var id = "#" + $(this).attr('id');
         setTotal(id);
+        
     });
         
 });
 
 
-
-//refactor after the quantity box is finished. Delete comment when finished!
-function getTotal(id){
-    var current_value = $(id).val();
-    var get_url = $(id).data('get-url') + "/" + current_value; 
-    $.getJSON(get_url, function(data){
-        var tests_html = '<div>$'+ (data.price / data.quantity).toFixed(2) +' ea.</div';
-        var final_piece = '$'+(data.price / data.quantity).toFixed(2)+'ea'
-        var total = '$'+data.price.toFixed(2);
-        $('.perPiece').html(tests_html);
-        $('.currentQuantity').html(data.quantity);
-        $('.finalPiece').html(final_piece);
-        $('#invitation_invitation_pricing_id').data("price", data.price);
-        $('.f-total').html(total);
-    });
+function finalPieceFormat(price, quantity){
+    formatted_price = '$'+(price / quantity).toFixed(2)+'ea'
+    return formatted_price;
 }
 
-function setTotal(id){
-    var current_value = $(id).val();
-    var get_url = $(id).data('get-url') + "/" + current_value;
-    getDropdownValue(get_url, id);   
+
+function perPieceFormat(price, quantity){
+    formatted_price = '<div>$'+ (price / quantity).toFixed(2) +' ea.</div';
+    return formatted_price;
 }
 
-function setQuantityBoxTotals(piece_price, current_quantity, final_piece, total){
-    $('.perPiece').html(piece_price);
-    $('.currentQuantity').html(current_quantity);
-    $('.finalPiece').html(final_piece);
-    $('.f-total').html(total);
-}
 
 function getDropdownValue(get_url, id){
     $.ajax({
@@ -61,37 +40,55 @@ function getDropdownValue(get_url, id){
     });
 }
 
-function perPieceFormat(price, quantity){
-    formatted_price = '<div>$'+ (price / quantity).toFixed(2) +' ea.</div';
-    return formatted_price;
-}
 
-function finalPieceFormat(price, quantity){
-    formatted_price = '$'+(price / quantity).toFixed(2)+'ea'
+function setBottomLineTotalOnlyWhenNoneValue(total){
+    $('.f-total').html(total);
 }
 
 
-function getCardTotal(id){
-    current_value = $(id).val();
-    get_url = $(id).data('get-url') + "/" + current_value;
-    ajaxify(get_url, id);
-}
-
-function ajaxify(get_url, id){
+function setBottomLineTotalOnly(get_url, id){
     $.ajax({
-        url: get_url,
+        url: get_url, 
         dataType: 'json',
-        success: function (json) {
+        success: function(json){
             $(id).data("price", json.price);
             total = sumAll();
-            price = $(id).data("price");
-            alert(total);
-            alert(price);
+            sumPriceAttributes();
+            $('.f-total').html(total);
         }
     });
 }
 
 
+function setQuantityBoxTotals(piece_price, current_quantity, final_piece, total){
+    $('.perPiece').html(piece_price);
+    $('.currentQuantity').html(current_quantity);
+    $('.finalPiece').html(final_piece);
+    $('.f-total').html(total);
+}
+
+
+function setTotal(id){
+    var current_value = $(id).val();
+
+    if ( current_value.length > 0 && id == "#invitation_invitation_pricing_id"){
+        
+        var get_url = $(id).data('get-url') + "/" + current_value;
+        getDropdownValue(get_url, id);
+        
+    }   else if ( current_value.length > 0 ){
+        var get_url = $(id).data('get-url') + "/" + current_value;
+        setBottomLineTotalOnly(get_url, id);
+    } else {
+        $(id).data("price", 0);
+        var total = sumAll();
+        setBottomLineTotalOnlyWhenNoneValue(total);
+        
+    }
+}
+
+// refactor this - this is stupid -- data price attribute only grabbing first data attribute when
+// using trying to aggregate with a single data call
 function sumAll(){
     var invitation_card_price = $("#invitation_invitation_pricing_id").data("price");
     var enclosure_card_price = $("#enclosure-cards_invitation_pricing_id").data("price");
@@ -110,3 +107,4 @@ function sumAll(){
                         wedding_menu_total).toFixed(2);
     return total;
 }
+
